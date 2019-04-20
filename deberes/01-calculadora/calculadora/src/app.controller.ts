9d8a1464070ec7b78ceb13d78045a865628ef651
Jacinto Palma <jxcinto12@gmail.com>
2019-04-20 15:54:18 -0500

    SUMA - RESTA
    Implementacion de la Suma y Resta

--- deberes/01-calculadora/calculadora/src/app.controller.ts
+++ deberes/01-calculadora/calculadora/src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post, HttpCode,Headers,Body,Response, Request, Delete,Put} from '@nestjs/common';
 import { AppService } from './app.service';
+import {response} from "express";
 
-@Controller()
+@Controller('/calculadora')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -9,4 +10,41 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // DEBER 1 - Aplicaciones Web
+  // Jacinto Palma Zambrano - Gr2
+
+
+  //Método GET - SUMA
+  @Get('/suma')
+  @HttpCode(200)
+  suma(@Headers() headers){
+  console.log('Headers: ', headers);
+  const numA = Number(headers.numero);
+  const numB = Number(headers.numero2);
+  const resp = numA + numB;
+  return "La suma de los números esresp" ;
+  }
+
+  //Método POST - RESTA
+  @Post('/resta')
+  @HttpCode(201)
+  resta(@Body() parametros, @Response() resp){
+  console.log(parametros);
+  if(parametros.numero && parametros.numero2){
+    const numA = Number(parametros.numero);
+    const numB = Number(parametros.numero2);
+    const numResta = numA-numB;
+    const salida = numResta.toString();
+    resp.set('Resta',`${salida}`);
+    return resp.status(201).send(`La resta es: ${numResta}`);
+  }else{
+    return resp.status(400).send("DEBE DE INGRESAR NUMEROS");
+  }
+
+
+
+
+  }
+
 }
