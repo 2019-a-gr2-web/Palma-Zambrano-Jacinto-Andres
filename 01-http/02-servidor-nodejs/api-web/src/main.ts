63016cfe9af97db9a6f5340a065d4d5eef5b6e1e
Jacinto Palma <jxcinto12@gmail.com>
2019-04-24 16:00:08 -0500

    FUNCIONES
    For Each, Map, Find, Filter

--- 01-http/02-servidor-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-nodejs/api-web/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const cookieParser = require('cookie-parser');
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {join} from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as NestExpressApplication;
   app.use(cookieParser('DANCE'));
+  app.set('view engine','ejs');
   await app.listen(3001);
 }
 bootstrap();
